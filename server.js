34d4c21fb967840a5392335128ad6264994f65fb
Chandra shekar <chandrashekar.mungi@gmail.com>
2017-08-27 11:47:10 +0530

    These project made by using Node Js , If you want run the project use this URI  http://localhost:8080
    1).Before running hitting the URI , you suppose to install all required files which we'included in the package json
    2). Then use these node servere js tags to run the COMMEND promontory.

---
+++ server.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var passport = require('passport');
+var Strategy = require('passport-facebook').Strategy;
+
+var userRouter  = express.Router();
+
+var CLIENT_ID = '1807453979283640' ;
+ var CLIENT_SECRET ='e7f5164314bdf36191c71e16b33efcb3' ;
+
+passport.use(new Strategy({
+    clientID: CLIENT_ID ,
+    clientSecret: CLIENT_SECRET ,
+    callbackURL: 'http://localhost:8080/login/facebook/return'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    return cb(null, profile);
+  }));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+var app = express();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Define routes.
+app.get('/',
+  function(req, res) {
+    res.render('home', { user: req.user });
+  });
+
+app.get('/login',
+  function(req, res){
+    res.render('login');
+  });
+
+app.get('/login/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/login/facebook/return', 
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.render('profile', { user: req.user });
+  });
+
+
+app.listen(8080,function(){
+  console.log("FB1-Server-Is-Up");
+});
